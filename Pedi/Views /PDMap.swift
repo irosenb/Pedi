6c94c05607a85fb052207c74ef29b6f5faef861b
Isaac Rosenberg <irosenb7@gmail.com>
2019-03-28 17:48:16 -0500

    added gitignore

---
+++ Pedi/Views /PDMap.swift
@@ -0,0 +1,13 @@
+//
+//  PDMap.swift
+//  Pedi
+//
+//  Created by Isaac Rosenberg on 3/21/19.
+//  Copyright © 2019 Isaac Rosenberg. All rights reserved.
+//
+
+import UIKit
+
+class PDMap: MGLMapView {
+
+}
