72e834420d2d39c6a03ae8df7269342ab158cd7e
Isaac Rosenberg <irosenb7@gmail.com>
2019-04-15 10:34:23 -0400

    PDServer file, to contain all the server info

---
+++ Pedi/Models/PDServer.swift
@@ -0,0 +1,13 @@
+//
+//  PDServer.swift
+//  Pedi
+//
+//  Created by Isaac Rosenberg on 4/14/19.
+//  Copyright © 2019 Isaac Rosenberg. All rights reserved.
+//
+
+import UIKit
+
+class PDServer: NSObject {
+  static let baseUrl = "http://192.168.1.5:3000"
+}
