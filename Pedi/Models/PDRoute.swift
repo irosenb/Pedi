9e00da44f9afd29fe046c18eae10dae00e9aec00
Isaac Rosenberg <irosenb7@gmail.com>
2019-04-24 15:22:51 -0400

    Calculate route

---
+++ Pedi/Models/PDRoute.swift
@@ -0,0 +1,36 @@
+//
+//  PDRoute.swift
+//  Pedi
+//
+//  Created by Isaac Rosenberg on 4/16/19.
+//  Copyright © 2019 Isaac Rosenberg. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+import MapboxDirections
+class PDRoute: NSObject {
+  public typealias RouteCompletionHandler = (_ route: Route?, _ error: NSError?) -> Void
+  
+  class func calculate(start: CLLocation, destination: CLLocation, completionHandler: @escaping RouteCompletionHandler) {
+    let waypoints = [
+      Waypoint(coordinate: start.coordinate, coordinateAccuracy: -1, name: "start"),
+      Waypoint(coordinate: destination.coordinate, coordinateAccuracy: -1, name: "end")
+    ]
+    
+    let options = RouteOptions(waypoints: waypoints, profileIdentifier: .cycling)
+    options.includesSteps = true
+    
+    let task = Directions.shared.calculate(options) { (waypoints, routes, error) in
+      guard error == nil else {
+        print("Error calculating directions: \(error!)")
+        completionHandler(nil, error)
+        return
+      }
+      
+      if let route = routes?.first, let leg = route.legs.first {
+        completionHandler(route, nil)
+      }
+    }
+  }
+}
